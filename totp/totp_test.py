61b7c413e285cbb59ff863a5816c0f946d790b93
pandong <pandong@hujiang.com>
2019-04-30 09:03:17 +0800

    调整secretKey的生成方式

--- totp/totp_test.py
+++ totp/totp_test.py
@@ -1,7 +1,7 @@
 import totp
 import base64
 
-secretKey = base64.b32encode(b'My secret key')
+secretKey = base64.b64encode(b'My secret key')
 t = totp.TOTP(secretKey, 4)
 time = t.tc(60)    # 此处时间单位为秒
 result=t.truncate(time)
